536c5bd0d94bc037a7db519c267ab8ce768db730
akashravindra029 <akashravindra029@gmail.com>
2025-08-22 11:46:16 +0530

    Delete src/Student_Management_System_Mini_Project directory

--- src/Student_Management_System_Mini_Project/DatabaseOperations.java
+++
@@ -1,8 +0,0 @@
-package Student_Management_System_Mini_Project;
-
-public interface DatabaseOperations {
-    void addStudent(Student student);
-    void updateStudent(int id, String newCourse, double newMarks);
-    void deleteStudent(int id);
-    void viewStudents();
-}
